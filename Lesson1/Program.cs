5b067ba722d1a08943f54a3ad0159b5c9752efa5
yanunes11 <yannunes112@gmail.com>
2022-12-23 12:50:12 -0300

    var const and variables

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -3,7 +3,17 @@
 namespace Lesson1{
     class Program{
         static void Main(string[] args){
+            const string myChannel = "MyChannel";
+            // myChannel = "OtherChannel"; //it doesnt work because is a constant
+            var varVariable = 9;
+            const double pi = 3.1415;
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("MyChannel: "+myChannel);
+            Console.WriteLine("varVariable: "+varVariable);
+            Console.WriteLine("pi: "+pi);
+            if (args.GetLength(0) > 0) {
+                Console.WriteLine(args.GetValue(0));
+            }
         }
     }
 }
