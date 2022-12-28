29ae27c445bbb4e7afdf4c5fd0cf0b01e468a1d9
yanunes11 <yannunes112@gmail.com>
2022-12-28 18:39:01 -0300

    params

---
+++ Lesson2/Basic3.cs
@@ -0,0 +1,37 @@
+/*
+    * @author : Yan Nascimento
+    * @Description : basic tools for using with C#
+    * @log : 28/12/2022 - created
+*/
+
+using System;
+
+namespace Lesson2 {
+    public class Basic3 {
+        //retrieve value from the user
+        public static void MainParams() {
+            ParamsArgument();
+            ParamsArgument(1);
+            ParamsArgument(1, 2, 7 ,3);
+        }
+        public static void ParamsArgument(params int[] n) {
+            int res=0;
+
+            if (n.Length < 1) {
+                Console.WriteLine("Any Param");
+            } else if (n.Length < 2) {
+                Console.WriteLine("No numbers enought");
+            } else {
+                foreach (int value in n) {
+                    res += value;
+                }
+                Console.WriteLine("The sum is: "+res);
+            }
+        }
+
+        public static void RandomCreation(int[] vector) {
+
+        }
+
+    }
+}
