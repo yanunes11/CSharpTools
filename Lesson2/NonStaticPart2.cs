b14c591e512006fbbfee75feb554babe11e136af
yanunes11 <yannunes112@gmail.com>
2023-01-02 15:46:22 -0300

    protected access modifier

--- Lesson2/NonStaticPart2.cs
+++ Lesson2/NonStaticPart2.cs
@@ -12,6 +12,9 @@ namespace Lesson2 {
         public NonStaticPart2() {
             // Inheritance // Gardening is a derived class
             Gardening2 gd = new Gardening2();
+
+            Computer cp = new Computer("Red", 1);
+            Console.WriteLine("Color: "+cp.getColor());
         }
 
     }
@@ -23,6 +26,8 @@ namespace Lesson2 {
         private int price = 0;
         private int quantity = 0;
 
+        protected string color;
+
         // constructor
         public Product2(int quantity) {
             this.quantity = quantity;
@@ -31,6 +36,10 @@ namespace Lesson2 {
 
         public string getPrice() {
             return name;
+        }
+
+        public string getColor() {
+            return color;
         }
 
         public void setPrice(int price) {
@@ -51,5 +60,10 @@ namespace Lesson2 {
         }
     }
 
+    public class Computer:Product2 {
+        public Computer(string color, int quantity):base(quantity) {
+            this.color = color;
+        }
+    }
 
 }
